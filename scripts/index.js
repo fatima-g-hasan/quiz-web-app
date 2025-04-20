fc4fa06fc936bbc3ccbc663d2ce968526ebb0d46
thecoder <fathas235@gmail.com>
2025-04-20 12:53:43 +0300

    logic of index page, add admin user and pass, and get form elements

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const adminEmail = "admin@quiz.com";
+const adminPassword = "admin123";
+
+const registerForm = document.getElementById("register-form");
+const loginForm = document.getElementById("login-form");
