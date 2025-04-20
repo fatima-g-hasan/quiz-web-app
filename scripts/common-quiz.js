2ed123a1218953480f144bccb55bee1ba23d125c
thecoder <fathas235@gmail.com>
2025-04-20 22:44:42 +0300

    get form and score elements, add correct answers of each test

---
+++ scripts/common-quiz.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("quiz-form");
+  const scoreDisplay = document.getElementById("score-output");
+
+  const answersKey = {
+    html: ["a", "b", "a", "c", "a"],
+    css: ["a", "a", "b", "a", "a"],
+    js: ["c", "a", "b", "b", "b"],
+  };
+});
