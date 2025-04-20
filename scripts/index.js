dbcf29931d134782d5cb3e9fdfe286f3202bdfbb
thecoder <fathas235@gmail.com>
2025-04-20 16:04:19 +0300

    get the values of email and pass, check for users in local storage

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,12 @@ const adminPassword = "admin123";
 
 const registerForm = document.getElementById("register-form");
 const loginForm = document.getElementById("login-form");
+
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById("register-email").value;
+  const password = document.getElementById("register-password").value;
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+});
